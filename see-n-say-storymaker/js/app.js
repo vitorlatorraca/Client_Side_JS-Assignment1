6530198daf6a84e1e1d36c0f6febe699859704cf
Vitor Latorraca <vitorlatorraca5@gmail.com>
2024-06-07 22:49:15 -0400

    new

--- see-n-say-storymaker/js/app.js
+++ see-n-say-storymaker/js/app.js
@@ -4,23 +4,78 @@
 -------------------------------------------------- */
 // Create a new speechSynthesis object
 var synth = window.speechSynthesis;
-// Learn more about SpeechSynthesis.speak() at https://developer.mozilla.org/en-US/docs/Web/API/SpeechSynthesis/speak
-var textToSpeak = 'This is the text string that you will generate with your script';
-var speakButton = document.querySelector('button');
 
+var textToSpeak = '';
+
+var subjects = ["The dog", "A cat", "A robot", "An alien", "A wizard"];
+var verbs = ["jumps", "runs", "flies", "eats", "sings"];
+var adjectives = ["happy", "sad", "fast", "slow", "angry"];
+var objects = ["a ball", "a sandwich", "a car", "a tree", "a book"];
+var places = ["in the park", "at home", "on the moon", "under the sea", "in a castle"];
+
+var subjectButton = document.getElementById('subjectButton');
+var verbButton = document.getElementById('verbButton');
+var adjectiveButton = document.getElementById('adjectiveButton');
+var objectButton = document.getElementById('objectButton');
+var placeButton = document.getElementById('placeButton');
+var speakButton = document.getElementById('speakButton');
+var randomStoryButton = document.getElementById('randomStoryButton');
+var resetButton = document.getElementById('resetButton');
+
+var story = document.getElementById('story');
 
 /* Functions
 -------------------------------------------------- */
+function getRandomElement(arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
+}
+
+function updateStory() {
+    textToSpeak = `${getRandomElement(subjects)} ${getRandomElement(verbs)} ${getRandomElement(adjectives)} ${getRandomElement(objects)} ${getRandomElement(places)}.`;
+    story.innerText = textToSpeak;
+}
+
 function speakNow(string) {
-	// Create a new speech object, attaching the string of text to speak
-	var utterThis = new SpeechSynthesisUtterance(string);
-	// Actually speak the text
-	synth.speak(utterThis);
+    var utterThis = new SpeechSynthesisUtterance(string);
+    synth.speak(utterThis);
 }
 
 /* Event Listeners
 -------------------------------------------------- */
-// Onclick handler for the button that speaks the text contained in the above var textToSpeak
+subjectButton.onclick = function() {
+    textToSpeak = getRandomElement(subjects);
+    story.innerText = textToSpeak;
+}
+
+verbButton.onclick = function() {
+    textToSpeak += ` ${getRandomElement(verbs)}`;
+    story.innerText = textToSpeak;
+}
+
+adjectiveButton.onclick = function() {
+    textToSpeak += ` ${getRandomElement(adjectives)}`;
+    story.innerText = textToSpeak;
+}
+
+objectButton.onclick = function() {
+    textToSpeak += ` ${getRandomElement(objects)}`;
+    story.innerText = textToSpeak;
+}
+
+placeButton.onclick = function() {
+    textToSpeak += ` ${getRandomElement(places)}.`;
+    story.innerText = textToSpeak;
+}
+
 speakButton.onclick = function() {
-	speakNow(textToSpeak);
+    speakNow(textToSpeak);
+}
+
+randomStoryButton.onclick = function() {
+    updateStory();
+}
+
+resetButton.onclick = function() {
+    textToSpeak = '';
+    story.innerText = '';
 }
